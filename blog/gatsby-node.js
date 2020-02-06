29ca23ee93892d036e7c48b5565a58a5d49eab3a
Nathan Johnson <johnsn98@gmatc.matc.edu>
2020-02-06 15:12:45 -0600

    contentful

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -1,26 +1,27 @@
 const path = require(`path`);
 
-exports.createPages = ({graphl, actions }) => {
+exports.createPages = ({graphql, actions }) => {
     const { createPage } = actions;
-    return new Promise((resolve, reject) ={
+    return new Promise((resolve, reject) => {
         graphql(`{
             allContentfulBlogPost {
-                edges {
-                  node {
-                    id
-                    slug
-                  }
+              edges {
+                node {
+                  id
+                  slug
                 }
               }
-        }`
+            }
+          }
+          `
       ).then(result => {
-          if(results.errors) {
-              reject(results.errors);
+          if(result.errors) {
+              reject(result.errors);
           }
           result.data.allContentfulBlogPost.edges.forEach((edge) => {
               createPage({
                   path: edge.node.slug,
-                  component: path.resolve(`./src/templaetes/blog-post.js`),
+                  component: path.resolve(`./src/templates/blog-post.js`),
                   context: {
                       slug: edge.node.slug
                   }
--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,26 +1,39 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from '../images/gatsby.jpg'
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home"
+    <SEO title="Home"/>
     
-    />
-    <h1>Theoretical Space Blog</h1>
-    <p>Welcome to my blog of space theories</p>
-   
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <img src={Image} alt='gatsby' />
-    </div>
-    <Link to="/page-2/"> Page-2 </Link>
-    <Link to="/about/"> about </Link>
-    <Link to="/contact/"> contact </Link>
+    <ul>
+    {
+      data.allContentfulBlogPost.edges.map(edge => (
+        <li>
+          <Link to={edge.node.slug} key={edge.node.id}>
+          {edge.node.slug}
+          </Link>
+        </li>
+
+      ))
+        }
+    </ul>
 
     </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+{
+  allContentfulBlogPost {
+    edges {
+      node {
+        id
+        slug
+      }
+    }
+  }
+}
+`
